231f20e82a39ec0cc053b6a5f10c60e90567e594
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-21 13:10:33 +0200

    select implementation

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,27 @@ import { useState, useEffect } from 'react'
 import Films from './assets/films'
 
 function App() {
-const [searchTxt, setSearchTxt] = useState('')
+  const [searchTxt, setSearchTxt] = useState('')
 
-const [filteredFilms , setFilteredFilms] = useState(Films)
+  const [filteredFilms, setFilteredFilms] = useState(Films)
 
-useEffect(()=>{
-  const filtered = Films.filter(({genre}) => genre.toLocaleLowerCase().includes(searchTxt.toLocaleLowerCase()))
-  setFilteredFilms(filtered)
-},[searchTxt, Films])
+  useEffect(() => {
+    const filtered = Films.filter(({ genre }) => genre.toLocaleLowerCase().includes(searchTxt.toLocaleLowerCase()))
+    setFilteredFilms(filtered)
+  }, [searchTxt, Films])
   return (
     <>
       <div className="container">
         <div className="container">
           <h1>FILMS</h1>
-          <input type="search" className='form-control me-2' placeholder='Search' aria-label='Search' onChange={event => setSearchTxt(event.target.value)} />
-          {filteredFilms.map((Film, index)=>{
+          <select className="form-select" aria-label="default select example" onChange={event => setSearchTxt(event.target.value)}>
+            <option value="">select genre...</option>
+            <option value="azione">Azione</option>
+            <option value="thriller">Thriller</option>
+            <option value="romantico">romantico</option>
+            <option value="fantascienza">Fantascienza</option>
+          </select>
+          {filteredFilms.map((Film, index) => {
             return (
               <li key={(index)}>{Film.title} {Film.genre}</li>
             )
