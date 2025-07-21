a33782d57ea0f2d86e41faf0302e0e2b163c4b35
FrancescoCellitti <francescocellitti@virgilio.it>
2025-07-21 12:53:40 +0200

    filter logic added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import Films from './assets/films'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [searchTxt, setSearchTxt] = useState('')
 
+const [filteredFilms , setFilteredFilms] = useState(Films)
+
+useEffect(()=>{
+  const filtered = Films.filter(({genre}) => genre.toLocaleLowerCase().includes(searchTxt.toLocaleLowerCase()))
+  setFilteredFilms(filtered)
+},[searchTxt, Films])
   return (
     <>
-      
+      <div className="container">
+        <div className="container">
+          <h1>FILMS</h1>
+          <input type="search" className='form-control me-2' placeholder='Search' aria-label='Search' onChange={event => setSearchTxt(event.target.value)} />
+          {filteredFilms.map((Film, index)=>{
+            return (
+              <li key={(index)}>{Film.title} {Film.genre}</li>
+            )
+          })}
+
+        </div>
+      </div>
     </>
   )
 }
